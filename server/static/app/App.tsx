2aa9f8a469c716449c6a02bf104e6412ad899cd5
Alexandr Yermak <ayermak@protonmail.ch>
2023-05-28 01:23:13 +0300

    mantine

--- server/static/app/App.tsx
+++ server/static/app/App.tsx
@@ -1,11 +1,17 @@
 import { Fade } from '@/animations';
+import { Button, Container, MantineProvider, Text } from '@mantine/core';
 
 export const App = () => {
   return (
-    <main>
-      <Fade>
-        <h1>Hello, world</h1>
-      </Fade>
-    </main>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>
+      <Container>
+        <main>
+          <Fade>
+            <Text height="xl">Hello, world</Text>
+            <Button>tap me :]</Button>
+          </Fade>
+        </main>
+      </Container>
+    </MantineProvider>
   );
 };
