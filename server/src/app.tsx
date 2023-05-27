3bef7ba0b52132c43a7599d4a7b8323ea5bd05e0
Alexandr Yermak <ayermak@protonmail.ch>
2023-05-27 04:51:33 +0300

    add eslint, prettier

--- server/src/app.tsx
+++ server/src/app.tsx
@@ -1,4 +1,7 @@
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.getElementById('root')!);
-root.render(<h1>Hello, world</h1>);
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<h1>Hello, world</h1>);
+}
