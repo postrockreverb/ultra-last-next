bded14212e773589d977364669e16921e4b0eacc
Alexandr Yermak <ayermak@protonmail.ch>
2023-06-03 19:58:36 +0300

    better live reload

--- server/static/rollup.config.js
+++ server/static/rollup.config.js
@@ -6,7 +6,6 @@ import terser from '@rollup/plugin-terser';
 import nodeGlobals from 'rollup-plugin-node-globals';
 import replace from '@rollup/plugin-replace';
 import external from 'rollup-plugin-peer-deps-external';
-import cleaner from 'rollup-plugin-cleaner';
 import autoprefixer from 'autoprefixer';
 import postcss from 'rollup-plugin-postcss';
 import pluginManifest from 'rollup-plugin-output-manifest';
@@ -19,26 +18,24 @@ const isDev = process.env.NODE_ENV === 'development';
 
 const extensions = ['.ts', '.tsx', '.js', '.jsx'];
 
+const watched = fs.readdirSync('./entries').map((entry) => `./dist/${entry}.js`);
 const entries = fs.readdirSync('./entries').reduce((entries, entry) => {
-  return {
-    ...entries,
-    [entry]: `./entries/${entry}/index.tsx`,
-  };
+  return { ...entries, [entry]: `./entries/${entry}/index.tsx` };
 }, {});
 
 export default {
   input: entries,
   output: {
     format: 'es',
-    entryFileNames: '[name]-[hash].js',
-    assetFileNames: '[name]-[hash].[extname]',
+    entryFileNames: isDev ? '[name].js' : '[name]-[hash].js',
+    assetFileNames: isDev ? 'assets/[name].[extname]' : 'assets/[name]-[hash].[extname]',
+    chunkFileNames: isDev ? 'chunks/[name].js' : 'chunks/dep-[hash].js',
     dir: './dist',
     exports: 'named',
     sourcemap: isDev,
     strict: true,
   },
   plugins: [
-    cleaner({ targets: ['./dist'] }),
     external(),
     resolve({
       extensions,
@@ -67,6 +64,6 @@ export default {
     !isDev && terser(),
     typescript({ tsconfig: './tsconfig.json' }),
     outputManifest({ filter: (chunk) => chunk.name && chunk.name in entries }),
-    isDev && livereload({ watch: './dist' }),
+    isDev && livereload({ watch: watched }),
   ],
 };
